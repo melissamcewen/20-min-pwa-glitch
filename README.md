7b40d02fcf24b37486fcfd957122951af3df12c1
Glitch (gatsby-20-min-pwa) <none>
2018-03-30 00:02:33 +0000

    🎻🎎 Updated with Glitch

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# 20-min-pwa-glitch
+# 20 Minute PWA Built In Gatsby
+This serves the final site using Express.js
+If you make changes you will need to "build"
+To do this go to [the console](https://glitch.com/faq#console) and type `gatsby build`
+
+[![Remix on Glitch](https://cdn.glitch.com/2703baf2-b643-4da7-ab91-7ee2a2d00b5b%2Fremix-button.svg)](https://glitch.com/edit/#!/remix/gatsby-20-min-pwa)
---
+++ gatsby-config.js
@@ -0,0 +1,41 @@
+module.exports = {
+  siteMetadata: {
+    title: 'Gatsby Default Starter',
+  },
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: "PWA20",
+        short_name: "PWA20",
+        start_url: "/",
+        "theme_color": "#6b37bf",
+        "background_color": "#6b37bf",
+        "display": "standalone",
+        icons: [
+          {
+            // Everything in /static will be copied to an equivalent
+            // directory in /public during development and build, so
+            // assuming your favicons are in /static/favicons,
+            // you can reference them here
+            src: `/favicons/android-chrome-192x192.png`,
+            sizes: `192x192`,
+            type: `image/png`,
+          },
+          {
+            src: `/favicons/android-chrome-256x256.png`,
+            sizes: `256x256`,
+            type: `image/png`,
+          },
+          {
+            src: `/favicons/android-chrome-512x512.png`,
+            sizes: `512x512`,
+            type: `image/png`,
+          }
+        ],
+      },
+    },
+    'gatsby-plugin-offline'
+  ],
+};
